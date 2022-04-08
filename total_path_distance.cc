d64e048c0d0b9beb6e830b892cbe758934d381ec
TABIBeck <103278850+TABIBeck@users.noreply.github.com>
2022-04-08 13:55:24 -0700

    Update total_path_distance.cc

--- total_path_distance.cc
+++ total_path_distance.cc
@@ -8,18 +8,18 @@ double Cities::total_path_distance(const permutation_t& ordering) const {
             std::sqr(coord1.second - coord2.second));
     }
     coord_t currentCity; // Current city will be set in loop automatically
-    coord_t lastCity = cityVect[ordering.front()];    // We set the last city as the
+    coord_t lastCity = cityVect_[ordering.front()];    // We set the last city as the
         // first so that it calculates a distance of 0 to get to the first
     double distance = 0;    // The total distance to traverse the cities in the
         // order given by the permutation
     for (c : ordering) {
-        currentCity = cityVect[c];
+        currentCity = cityVect_[c];
         distance += pair_dist(currentCity, lastCity);    // Adds the distance
                 // between the new point and the last
         lastCity = currentCity; // Moves the current city to the last city for
             // the next step
     }
-    distance += pair_dist(currentCity, cityVect[ordering.front()]); // Finally,
+    distance += pair_dist(currentCity, cityVect_[ordering.front()]); // Finally,
         // adds the distance from the last city back to the first
     return distance;
 }
