5d8cb1479247a3a469789b387107086e4780cff7
mboorj <94495260+mboorj@users.noreply.github.com>
2022-04-09 07:13:58 -0700

    Sketch of tsp.cc, no I/O operations added yet

--- tsp.cc
+++ tsp.cc
@@ -1,5 +1,24 @@
 #include "cities.hh"
 
 int main(){
-  return 0;
+  Cities map;
+  // read cities in from file somehow
+  auto shortestLen = map.total_path_distance(); // print "0" + tab + len
+  auto shortestRoute = map;
+
+  int someMax;
+  int i = 1;
+  int len = map.return_cities().size();
+  while (i < someMax){
+    auto newPerm = random_permutation(len)
+    auto newCities = map.reorder(newPerm);
+    auto newLength = newCities.total_path_distance();
+    if (newLength <= shortestLen){
+      shortestLen = newLength;
+      shortestRoute = newCities;
+      // print i + tab + len
+    }
+    i++;
+  }
+  // when this loop is done, read shortestRoute out to a file somehow
 }
